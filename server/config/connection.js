fe7cfa1dde30bc8ff8382b909512437dff45b155
Anthony Bradford <agb2152001@gmail.com>
2023-09-18 21:51:28 -0400

    added spacing in history

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,14 +2,16 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/fitway", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}, err => {
-  if (err) throw err;
-  console.log('Connected to MongoDB!')
-}
-
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/fitway",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to MongoDB!");
+  }
 );
 
 module.exports = mongoose.connection;
